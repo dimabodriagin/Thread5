e93b9c7e811ad69ef1aa0cd6df54b6916b12ea70
Dima <bodryi2001@icloud.com>
2024-06-01 21:55:32 +0400

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Name";
+        String number = "+79452007010";
+
+        int result = phoneBook.add(name, number);
+        Map<String, String> phoneMap = new TreeMap<>();
+        phoneMap.put(name, number);
+
+        Assertions.assertEquals(result, phoneMap.size());
+    }
 }
