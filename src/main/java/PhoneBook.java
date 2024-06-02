4c0349e13a7582eb865a65fea33f9ca428394b33
Dima <bodryi2001@icloud.com>
2024-06-02 19:28:03 +0400

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,15 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
 
+    private Map<String, String> phoneBook;
+
     public int add(String name, String number) {
-        return 0;
+        if (phoneBook == null) {
+            phoneBook = new TreeMap<>();
+        }
+        phoneBook.put(name, number);
+        return phoneBook.size();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
